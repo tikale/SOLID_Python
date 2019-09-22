9c2e8561018681dc856f94b43c24ce2c5333e3fe
tikale <lekawa77@gmail.com>
2019-09-22 23:15:31 +0200

    Liskov & InterfaceSegragation

--- SOLID_InterfaceSegregation.py
+++ SOLID_InterfaceSegregation.py
@@ -3,3 +3,36 @@
 #		Interfejsy powinny być niewielkie (niewiele metod).
 #		Nie powinny mieć więcej niż klasy które obsługują.
 #		Powinny mieć jedną odpowiedzialność.
+
+# Jeśli chodzi o pythona to nie wspiera on interfejsów jako takich.
+# Interface Segregation Principle możemy tu pokazać jako dziedziczenie po klasie abstrakcyjnej
+
+# Źle
+class Shape_OC:
+	def __init__(self, name: str):
+		self.name = name
+		
+	def get_name(self):
+		return self.name
+		
+	def Draw_Kwadrat(self):
+		pass
+		
+	def Draw_Okrag(self):
+		pass
+		
+	def Draw_Trojkat(self):
+		pass
+		
+		
+class Kwadrat(Shape_OC):
+	def Draw_Kwadrat(self):
+		return 'Kwadrat'
+
+class Okrag(Shape_OC):
+	def Draw_Okrag(self):
+		return 'Okrag'
+
+class Trojkat(Shape_OC):
+	def Draw_Trojkat(self):
+		return 'Trojkat'
--- SOLID_LiskovSubstitution.py
+++ SOLID_LiskovSubstitution.py
@@ -2,3 +2,37 @@
 #		Jeśli S jest podtypem T, to obiekty T mogą być zastępowane obiektami typu S bez modyfikowania funkcjonalności programu. 
 #		np. klasa ptak ma funkcje lataj, pingwin dziedziczy po klasie ptak, ale ponieważ jest nielotem funkcja lataj dla pingwina nie ma sensu.
 #		Dlatego wramach zgodności z zasadą Liskov. Pingwin nie może dziedziczyć po klasie ptak.
+
+class Bird:
+	def __init__(self):
+		pass
+		
+	def fly(self):
+		pass
+
+	def eat(self):
+		pass
+
+class Penguin(Bird):  # Pingwin nie lata więc odziedziczona metoda fly nie będzie używana.	 
+	pass
+	
+class Sparrow(Bird):  # Możemy użyć wszystkich funkcji z klasy Bird 
+	pass
+	
+class Animal:
+		def __init__(self):
+			pass
+			
+		def eat(self):
+			pass
+			
+class Penguin(Animal):  # Pingwin nie lata więc odziedziczona metoda fly nie będzie używana.	 
+		def __init__(self):
+			pass
+			
+		def fly(self):
+			pass
+	
+class Sparrow(Animal):  # Możemy użyć wszystkich funkcji z klasy Bird 
+	pass
+
