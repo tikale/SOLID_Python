4e04f75abffb465a3cbab7acc1d00f304abeb72a
tikale <lekawa77@gmail.com>
2019-10-13 22:16:55 +0200

    SOLID_DependencyInversion

--- SOLID_DependencyInversion.py
+++ SOLID_DependencyInversion.py
@@ -5,3 +5,38 @@
 #		Szczeguły powinny zeleżeć od abstrakcji. 
 #		Moduły wysokiego poziomu mówią nam co program ma robić (logika biznesowa). 
 #		Niskiego poziomu w jaki sposób mają to robić.
+
+class Engine:
+	def SetEngine(self, x):
+	if x == 0:
+		print('Stop Engine')
+	elif x == 1:
+		print('move right')
+	elif x == -1:
+		print('move left')
+	
+
+class EngineControl(Engine):
+	def positive(self):
+		super().SetEngine(1)
+	def negative(self):
+		super().SetEngine(-1)
+	def Stop(self):
+		super().SetEngine(0)
+		
+class ProbeProgram(EngineControl):
+	# glebokosc
+	def Probe(self):
+			#na dno
+			#stop
+			#na powierzchnie
+
+
+
+Obj1 = PrintVariable()
+Obj1.printX(5)
+
+Obj2 = Dzialania()
+Obj2.Dodaj(x)
+
+		
